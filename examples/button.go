f01b099a6bdad3359a835146cbaad3d65ea235bd
Danil Borchevkin <danil.borchevkin@lab409.ru>
2017-07-30 00:47:02 +0200

    Minor fixes

--- examples/button.go
+++ examples/button.go
@@ -5,19 +5,19 @@ import "time"
 import "github.com/lab409/go-artik/gpio"
 
 func main() {
-	fmt.Println("\nExample. Set gpio125 (Arduino 8 pin) if gpio124 is HIGH")
+	fmt.Println("\nExample. Set gpio125 (Arduino 8 pin) if gpio124 (Arduino 7 pin) is HIGH")
 	fmt.Println("For exit press Ctrl+C\n")
 
 	/* Create pins for led and button */
-	led := gpio.NewPin(125, gpio.GPIO_OUT)
-	button := gpio.NewPin(124, gpio.GPIO_IN)
+	led := gpio.NewPin(125, gpio.OUT)
+	button := gpio.NewPin(124, gpio.IN)
 	
 	/* Clear the led */
 	led.Clear()
 
 	/* Infinite loop with polling of the button state and setting led */
 	for {
-		if (button.GetState() == gpio.GPIO_HIGH) {
+		if (button.GetState() == gpio.HIGH) {
 			led.Set()
 		} else {
 			led.Clear()
